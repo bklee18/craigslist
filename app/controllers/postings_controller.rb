45f6a623a3a85415115245eb05a8a940075a2061
Brandon Wu & Brian Lee <pair+beewuu.bklee18@devbootcamp.com>
2016-07-11 17:57:51 -0400

    add custom css

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -8,6 +8,10 @@ class PostingsController < ApplicationController
   end
 
   def show
+    @state = State.find(params[:state_id])
+    @city = City.find(params[:city_id])
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:subcategory_id])
     @posting = Posting.find(params[:id])
   end
 
@@ -18,9 +22,50 @@ class PostingsController < ApplicationController
     @subcategory = Subcategory.find(params[:subcategory_id])
     @posting = Posting.new(posting_params)
     if @posting.save
+      flash[:notice] = @posting.edit_key
       redirect_to state_city_category_subcategory_posting_path(@state, @city, @category, @subcategory, @posting)
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @state = State.find(params[:state_id])
+    @city = City.find(params[:city_id])
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:subcategory_id])
+    @posting = Posting.find(id_params)
+    if params[:key] == @posting.edit_key
+      render 'edit'
+    else
+      render not_found
+    end
+  end
+
+  def destroy
+    @state = State.find(params[:state_id])
+    @city = City.find(params[:city_id])
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:subcategory_id])
+    @posting = Posting.find(id_params)
+    if params[:key] == @posting.edit_key
+      @posting.destroy
+      redirect_to '/'
+    else
+      render not_found
+    end
+  end
+
+  def update
+    @state = State.find(params[:state_id])
+    @city = City.find(params[:city_id])
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.find(params[:subcategory_id])
+    @posting = Posting.find(id_params)
+    if Posting.update(@posting.id, posting_params)
+      redirect_to state_city_category_subcategory_posting_path(@state, @city, @category, @subcategory, @posting)
+    else
+      render not_found
     end
   end
 
@@ -28,5 +73,9 @@ class PostingsController < ApplicationController
 
   def posting_params
     params.require(:posting).permit(:title, :description, :email)
+  end
+
+  def id_params
+    params.require(:id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  root 'states#index'
   resources :states do
     resources :cities do
       resources :categories do
         resources :subcategories do
-          resources :postings
+          resources :postings, except: [:destroy, :edit]
         end
       end
     end
   end
+
+
+
+  get '/states/:state_id/cities/:city_id/categories/:category_id/subcategories/:subcategory_id/postings/:id/edit' => 'postings#edit'
+  put '/states/:state_id/cities/:city_id/categories/:category_id/subcategories/:subcategory_id/postings/:id' => 'postings#update'
+  delete '/states/:state_id/cities/:city_id/categories/:category_id/subcategories/:subcategory_id/postings/:id/delete' => 'postings#destroy'
 end
+
